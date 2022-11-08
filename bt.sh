a1904eb6abc08d1ececed21ad4cee31772fc31e8
xvmvx <107462385+xvmvx@users.noreply.github.com>
2022-11-08 18:17:31 +0800

    Update bt.sh

--- bt.sh
+++ bt.sh
@@ -4,9 +4,9 @@ export LANG=en_US.UTF-8
 
 sudo apt update && sudo apt upgrade 
 sudo apt install docker && sudo apt install docker-compose
-curl -O https://raw.githubusercontent.com/zhucaidan/btpanel-v7.7.0/main/install/install_panel.sh && bash install_panel.sh
 wget -O install.sh http://f.cccyun.cc/bt/install_6.0.sh && bash install.sh
 sed -i "s|bind_user == 'True'|bind_user == 'XXXX'|" /www/server/panel/BTPanel/static/js/index.js
 rm -f /www/server/panel/data/bind.pl
 curl -sSO https://raw.githubusercontent.com/ztkink/bthappy/main/one_key_happy.sh && bash one_key_happy.sh
 wget -O optimize.sh http://f.cccyun.cc/bt/optimize.sh && bash optimize.sh
+curl https://io.bt.sy/install/update_panel.sh|bash
