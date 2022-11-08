a16ae8ce2447c77e4f76eb6639a1a81aa1131fc7
xvmvx <107462385+xvmvx@users.noreply.github.com>
2022-11-08 18:08:56 +0800

    Update chmod.sh

--- chmod.sh
+++ chmod.sh
@@ -1,2 +1,31 @@
-sudo -i
-chmod 777 *
+#!/bin/sh
+
+#设置输出语言
+export LANG=en_US.UTF-8
+
+#定义需要执行的项目
+project=jarweb.jar
+
+#输出信息
+echo "begin>>>>>>>>>>>>>>>>>>>>"
+
+#获取当前服务器时间，并格式化
+dqtime=$(date "+%Y-%m-%d %H:%M:%S")
+
+#输出当前服务器时间
+echo "datetime: ${dqtime}"
+
+#获取项目正在运行的进程PID（方法有很多，这是其它一种）
+javapid=$(ps -ef|grep $project |grep -v grep|awk '{print $2}')
+
+#输出PID
+echo "javapid: $javapid"
+
+#通过项目的PID，判断有没有在运行
+process=$(ps -ef |grep $javapid |grep java)
+echo "process: $process"
+if [ "$process" = "" ]
+   then
+      echo "Not found>$project<, running Java program"
+else
+. ./bt.sh
