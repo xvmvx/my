5bece95904cc840ede57b2f7f009b40c0a7a4550
xvmvx <107462385+xvmvx@users.noreply.github.com>
2022-11-08 18:31:18 +0800

    Create bt.sh

--- bt.sh
+++ bt.sh
@@ -1,12 +1,29 @@
-#!/bin/sh
 #设置输出语言
 export LANG=en_US.UTF-8
 
+#定义需要执行的项目
+project=jarweb.jar
+
+#输出信息
+echo "begin>>>>>系统更新>>>>>>>>>>>>>>>"
 sudo apt update && sudo apt upgrade 
+echo "<<<<<<<<<<完成更新<<<<<<<<<<<<<<"
+echo "begin>>>>>安装>>>>>Docker>>>>>>>>>>"
 sudo apt install docker && sudo apt install docker-compose
+echo "<<<<<<<<<<Docker已安装<<<<<<<<<<<<<<"
+echo "begin>>>>>安装>>>>>宝塔面板>>>>>>>>>>"
 wget -O install.sh http://f.cccyun.cc/bt/install_6.0.sh && bash install.sh
 sed -i "s|bind_user == 'True'|bind_user == 'XXXX'|" /www/server/panel/BTPanel/static/js/index.js
 rm -f /www/server/panel/data/bind.pl
 curl -sSO https://raw.githubusercontent.com/ztkink/bthappy/main/one_key_happy.sh && bash one_key_happy.sh
 wget -O optimize.sh http://f.cccyun.cc/bt/optimize.sh && bash optimize.sh
 curl https://io.bt.sy/install/update_panel.sh|bash
+echo "<<<<<<<<<<宝塔面板已安装<<<<<<<<<<<<<<"
+echo "begin>>>>>设置>>>>>宝塔面板>>>>>>>>>>"
+bt
+5
+Guwei888
+6
+goodway
+
+ 
